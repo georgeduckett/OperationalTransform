ff76b04ab9655ed15034a2e1a3b588b7c8820c09
George Duckett <george.duckett@aqualisa.co.uk>
2017-06-08 15:59:34 +0100

    Added CP1/TP1 test

--- OperationalTransform.Tests/OperationTransformer/TransformConvergencePropertiesTests.cs
+++ OperationalTransform.Tests/OperationTransformer/TransformConvergencePropertiesTests.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OperationalTransform.Operations;
+using OperationalTransform.StateManagement;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +18,21 @@ namespace OperationalTransform.Tests
         [TestMethod]
         public void OperationTransformer_Transform_CP1TP1Satisfied()
         {
-            Assert.Inconclusive("Not yet checked");
+            var site1Str = "abcd";
+            var site2Str = "abcd";
+            var site1 = new SiteState(1, site1Str);
+            var site2 = new SiteState(2, site2Str);
+
+            var op1 = new InsertOperation(site1, 0, '1');
+            var op2 = new InsertOperation(site2, 0, '2');
+
+            site1Str = op2.ApplyTransform(site1Str);
+            site1Str = OperationTransformer.Transform(op1, op2).ApplyTransform(site1Str);
+
+            site2Str = op1.ApplyTransform(site2Str);
+            site2Str = OperationTransformer.Transform(op2, op1).ApplyTransform(site2Str);
+
+            Assert.AreEqual(site1Str, site2Str);
         }
         [TestMethod]
         public void OperationTransformer_Transform_CP2TP2Satisfied()
